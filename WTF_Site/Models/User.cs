d509ea0ea0154bda9f30593051305bbe5da82557
SUPACOOL <steveash@whatsthatfunk.com>
2017-02-12 00:05:27 -0600

    First checkin to git.

---
+++ WTF_Site/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WTF_Site.Models
+{
+    public class User
+    {
+    }
+}
---
+++ WTF_Site/ViewModels/ComicViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WTF_Site.Models;
+
+namespace WTF_Site.ViewModels
+{
+    public class ComicViewModel
+    {
+        public Comic Comic;
+        public User User;
+    }
+}
